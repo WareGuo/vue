9fac6d772543cc8fc226cb3ab104bc8e9c53cb36
WareGuo <5226969@qq.com>
2019-04-22 10:54:52 +0800

    addnewfunction

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, make_response
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Welcome to my app!"
+    places=request.cookies.get('places')
+    return render_template("index.html",places=places)
+
+@app.route("/add", methods=["POST"])
+def add():
+    temp = redirect(url_for("index"))
+    response = make_response(temp)
+    place = request.form.get("place")
+    places=request.cookies.get('places')
+    response.set_cookie("places", places + place, expires=100000000)
+    return response
 
 if __name__ == "__main__":
     app.run()

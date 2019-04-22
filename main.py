1bdf766f0a2ec4b8034f48b8d855df25a9f249a0
WareGuo <5226969@qq.com>
2019-04-22 11:27:08 +0800

    modifymain.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, make_response, session
 import os
 from datetime import timedelta
+import random
 
 app = Flask(__name__)
 app.config['SECRET_KEY']=os.urandom(24)
@@ -8,18 +9,28 @@ app.config['PERMANENT_SESSION_LIFETIME']=timedelta(days=7)
 
 @app.route("/")
 def index():
-    places=session.get('places')
-    return render_template("index.html",places=places)
+    placesList=session.get('places').split("@")
+    return render_template("index.html",places=placesList)
 
 @app.route("/add", methods=["POST"])
 def add():
     place = request.form.get("place")
     places = session.get('places')
     if(places):
-        session['places'] = places + place
+        session['places'] = places + "@" +place
     else:
         session['places'] = place
     return redirect(url_for("index"))
+
+@app.route("/output")
+def output():
+    places = session.get('places')
+    if(places):
+        placesList = places.split("@")
+        a = random.sample(placesList, 1)
+        return str(a)
+    else:
+        return "暂无数据"
 
 if __name__ == "__main__":
     app.run()

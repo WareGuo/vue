5d89780ef01fcc4eb94aa25f5a64740a612a708a
WareGuo <5226969@qq.com>
2019-04-22 11:09:24 +0800

    modifymain.py

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
-from flask import Flask, render_template, request, redirect, url_for, make_response
+from flask import Flask, render_template, request, redirect, url_for, make_response, session
+import os
+from datetime import timedelta
 
 app = Flask(__name__)
+app.config['SECRET_KEY']=os.urandom(24)
+app.config['PERMANENT_SESSION_LIFETIME']=timedelta(days=7)
 
 @app.route("/")
 def index():
-    places=request.cookies.get('places')
+    places=session.get('places')
     return render_template("index.html",places=places)
 
 @app.route("/add", methods=["POST"])
 def add():
-    places=request.cookies.get('places')
-    temp = render_template("index.html",places=places)
-    response = make_response(temp)
     place = request.form.get("place")
-    if(places):
-        place += places
-    response.set_cookie("places", place, expires=100000000)
-    return response
+    places = session.get('places')
+    session['places'] = places + place
+    return redirect(url_for("index"))
 
 if __name__ == "__main__":
     app.run()
